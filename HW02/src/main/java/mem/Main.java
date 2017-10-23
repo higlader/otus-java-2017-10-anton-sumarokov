6ebbb81d10d84b0b9047a51adc42c1e9499f7e0d
higlader <wton1984@mail.ru>
2017-10-23 15:54:55 +0300

    "DZ02-clear

--- HW02/src/main/java/mem/Main.java
+++ HW02/src/main/java/mem/Main.java
@@ -20,21 +20,30 @@ public class Main {
     public static void main(String... args) throws InterruptedException {
         System.out.println("pid: " + ManagementFactory.getRuntimeMXBean().getName());
 
-        int size = 20_000_000;
+        int size = 1000;
 
         System.out.println("Starting the loop");
+        Runtime runtime = Runtime.getRuntime();
+        System.gc();
+        long mem = runtime.totalMemory() - runtime.freeMemory();
+
         while (true) {
             Object[] array = new Object[size];
             System.out.println("New array of size: " + array.length + " created");
             for (int i = 0; i < size; i++) {
-                array[i] = new Object();
-                //array[i] = new String(""); //String pool
+                //array[i] = new Object();
+                array[i] = new String(""); //String pool
                 //array[i] = new String(new char[0]); //without String pool
                 //array[i] = new MyClass();
             }
             System.out.println("Created " + size + " objects.");
+            System.gc();
+            long mem1 = runtime.totalMemory() - runtime.freeMemory();
+            long total = (mem1 - mem) / size;
+            System.out.println("Объект равен: " + total + "Byte");
             Thread.sleep(1000); //wait for 1 sec
         }
+
     }
 
     private static class MyClass {
