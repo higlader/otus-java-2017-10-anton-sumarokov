12f401719a8b13403f9d3fb4518242e51c088812
higlader <wton1984@mail.ru>
2017-10-23 14:54:02 +0300

    "instrumentation"

---
+++ HW02/src/main/java/mem/ObjectSizeFetcher.java
@@ -0,0 +1,17 @@
+package mem;
+import java.lang.instrument.Instrumentation;
+
+/**
+ * Created by anton on 23.10.17.
+ */
+public class ObjectSizeFetcher {
+    private static Instrumentation instrumentation;
+
+    public static void premain(String args, Instrumentation inst) {
+        instrumentation = inst;
+    }
+
+    public static long getObjectSize(Object o) {
+        return instrumentation.getObjectSize(o);
+    }
+}
