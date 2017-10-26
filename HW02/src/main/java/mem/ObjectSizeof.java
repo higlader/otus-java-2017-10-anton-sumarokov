4a4b372f1a153f55b911e43a86b991b24308a76f
higlader <wton1984@mail.ru>
2017-10-26 14:20:12 +0300

    "DZ02-pravka03"

--- HW02/src/main/java/mem/ObjectSizeof.java
+++ HW02/src/main/java/mem/ObjectSizeof.java
@@ -1,7 +1,33 @@
 package mem;
 
+
+import java.lang.reflect.Array;
+
 /**
  * Created by anton on 24.10.17.
  */
 public class ObjectSizeof {
+
+    public static void getSize(Object[] array) throws InterruptedException {
+        Runtime runtime = Runtime.getRuntime();
+        System.gc();
+        int size = Array.getLength(array);
+        Object[] obj = new Object[size];
+        long mem = runtime.totalMemory() - runtime.freeMemory();
+
+        while (true) {
+            System.out.println("New array of size: " + obj.length + " created");
+            for (int i = 0; i < size; i++) {
+                obj[i] = new Object[i];
+            }
+            System.out.println("Created " + size + " objects.");
+            long mem1 = runtime.totalMemory() - runtime.freeMemory();
+            System.gc();
+            long total = (mem1 - mem) / size;
+            System.out.println("Объект равен: " + total + "Byte");
+            Thread.sleep(1000); //wait for 1 sec
+        }
+    }
 }
+
+
