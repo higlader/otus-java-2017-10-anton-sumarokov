a8241d78786023385f5fb21d6f71d492d5bd6bd8
higlader <wton1984@mail.ru>
2017-10-30 16:39:25 +0300

    "DZ02-pravka05"

--- HW02/src/main/java/mem/ObjectSizeof.java
+++ HW02/src/main/java/mem/ObjectSizeof.java
@@ -2,31 +2,35 @@ package mem;
 
 
 import java.lang.reflect.Array;
+import java.util.function.Supplier;
 
 /**
  * Created by anton on 24.10.17.
  */
-public class ObjectSizeof {
+public class ObjectSizeof<T> {
+    int size;
 
-    public static void getSize(Object[] array) throws InterruptedException {
+    public ObjectSizeof(int size) {
+        this.size = size;
+    }
+
+    public  <T> T getSize(Supplier<T> supplier) {
+        Object[] obj = new Object[size];
         Runtime runtime = Runtime.getRuntime();
-        System.gc();
-        int size = Array.getLength(array);
+        runtime.gc();
         long mem = runtime.totalMemory() - runtime.freeMemory();
 
-        while (true) {
-            Object[] obj = new Object[size];
-            System.out.println("New array of size: " + obj.length + " created");
-            for (int i = 0; i < size; i++) {
-                obj[i] = new Object[i];
-            }
-            System.out.println("Created " + size + " objects.");
-            long mem1 = runtime.totalMemory() - runtime.freeMemory();
-            System.gc();
-            long total = (mem1 - mem) / size;
-            System.out.println("Объект равен: " + total + "Byte");
-            Thread.sleep(1000); //wait for 1 sec
+        System.out.println("New array of size: " + obj.length + " created");
+        for (int i = 0; i < size; i++) {
+            obj[i] = supplier.get();
         }
+        runtime.gc();
+        System.out.println("Created " + size + " objects.");
+        long mem1 = runtime.totalMemory() - runtime.freeMemory();
+        long total = (mem1 - mem);
+        System.out.println("Объект равен: " + total + "Byte");
+        //Thread.sleep(1000); //wait for 1 sec
+        return (T) obj;
     }
 }
 
