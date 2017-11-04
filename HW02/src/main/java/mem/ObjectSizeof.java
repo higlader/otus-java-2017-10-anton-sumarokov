8e8a4bd5f98f845606179af4bbd68c5d95295dec
anton <wton1984@mal.ru>
2017-11-04 16:13:19 +0300

    "DZ02-pravka12"

--- HW02/src/main/java/mem/ObjectSizeof.java
+++ HW02/src/main/java/mem/ObjectSizeof.java
@@ -1,22 +1,16 @@
 package mem;
 
 
-import java.lang.reflect.Array;
 import java.util.function.Supplier;
 
 /**
  * Created by anton on 24.10.17.
  */
-public class ObjectSizeof<T> {
-    int size;
-    Object type;
+public class ObjectSizeof {
+    int size = 1_000_000;
 
-    public ObjectSizeof(int size, Object type) {
-        this.size = size;
-        this.type = type;
-    }
 
-    public  <T> Object getSize(Supplier<T> supplier) {
+    public  <T> Object getSize(Supplier<Object> supplier) {
         Object[] obj = new Object[size];
         Runtime runtime = Runtime.getRuntime();
         runtime.gc();
@@ -30,7 +24,8 @@ public class ObjectSizeof<T> {
         System.out.println("Created " + size + " objects.");
         long mem1 = runtime.totalMemory() - runtime.freeMemory();
         long total = (mem1 - mem)/size;
-        return ("Объект равен: " + total + "Byte");
+        String name = supplier.get().getClass().getCanonicalName();
+        return ("Объект " + name + " равен: " + total + "Byte");
     }
 }
 
