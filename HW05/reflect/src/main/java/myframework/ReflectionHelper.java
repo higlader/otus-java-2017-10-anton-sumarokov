9fb3d61a5c63b01ae85e8f81d729909ba145ecb6
higlader <wton1984@mail.ru>
2017-11-22 13:36:24 +0300

    DZ05-begin

---
+++ HW05/reflect/src/main/java/myframework/ReflectionHelper.java
@@ -0,0 +1,152 @@
+package myframework;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Created by tully.
+ */
+@SuppressWarnings("SameParameterValue")
+class ReflectionHelper {
+    private ReflectionHelper() {
+    }
+
+    static <T> T instantiate(Class<T> type, Object... args) {
+        try {
+            if (args.length == 0) {
+                return type.newInstance();
+            } else {
+                return type.getConstructor(toClasses(args)).newInstance(args);
+            }
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    static Object getFieldValue(Object object, String name) {
+        Field field = null;
+        boolean isAccessible = true;
+        try {
+            field = object.getClass().getDeclaredField(name); //getField() for public fields
+            isAccessible = field.isAccessible();
+            field.setAccessible(true);
+            return field.get(object);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        } finally {
+            if (field != null && !isAccessible) {
+                field.setAccessible(false);
+            }
+        }
+        return null;
+    }
+
+    static List<Method> getMethodsByAnnotation(Class<?> type, Class<? extends Annotation> annotation) {
+        List<Method> result = new ArrayList<>();
+        List<Method> methods = new ArrayList<>(Arrays.asList(type.getDeclaredMethods()));
+
+        for(Method m : methods) {
+            if(m.isAnnotationPresent(annotation)){
+                result.add(m);
+            }
+        }
+
+        return result;
+    }
+
+    static void setFieldValue(Object object, String name, Object value) {
+        Field field = null;
+        boolean isAccessible = true;
+        try {
+            field = object.getClass().getDeclaredField(name); //getField() for public fields
+            isAccessible = field.isAccessible();
+            field.setAccessible(true);
+            field.set(object, value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        } finally {
+            if (field != null && !isAccessible) {
+                field.setAccessible(false);
+            }
+        }
+    }
+
+    static Object callMethod(Object object, String name, Object... args) {
+        Method method = null;
+        boolean isAccessible = true;
+        try {
+            method = object.getClass().getDeclaredMethod(name, toClasses(args));
+            isAccessible = method.isAccessible();
+            method.setAccessible(true);
+            return method.invoke(object, args);
+        } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        } finally {
+            if (method != null && !isAccessible) {
+                method.setAccessible(false);
+            }
+        }
+        return null;
+    }
+
+    static private Class<?>[] toClasses(Object[] args) {
+        List<Class<?>> classes = Arrays.stream(args)
+                .map(Object::getClass)
+                .collect(Collectors.toList());
+        return classes.toArray(new Class<?>[classes.size()]);
+    }
+
+    static Class[] getAllClassesInPackage(String packageName){
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        assert classLoader != null;
+        String path = packageName.replace('.', '/');
+        Enumeration<URL> resources = null;
+        try {
+            resources = classLoader.getResources(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<File> dirs = new ArrayList<>();
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            dirs.add(new File(resource.getFile()));
+        }
+        ArrayList<Class> classes = new ArrayList<>();
+        for (File directory : dirs) {
+            try {
+                List<Class> foundClasses = findClasses(directory, packageName);
+                classes.addAll(foundClasses);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        return classes.toArray(new Class[classes.size()]);
+    }
+
+    private static List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
+        List<Class> classes = new ArrayList<>();
+        if (!directory.exists()) {
+            return classes;
+        }
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                assert !file.getName().contains(".");
+                classes.addAll(findClasses(file, packageName + "." + file.getName()));
+            } else if (file.getName().endsWith(".class")) {
+                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+            }
+        }
+        return classes;
+    }
+}
---
+++ HW05/reflect/src/main/java/myframework/annotations/After.java
@@ -0,0 +1,13 @@
+package myframework.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by anton on 22.11.17.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+@Inherited
+public @interface After {
+
+}
