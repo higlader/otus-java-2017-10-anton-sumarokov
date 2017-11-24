1cc62eaf9c59ac87b2f3b82946317400cb8d97cb
higlader <wton1984@mail.ru>
2017-11-24 11:30:04 +0300

    Update Main.java

--- HW04/bench/src/main/java/bench/Main.java
+++ HW04/bench/src/main/java/bench/Main.java
@@ -11,8 +11,8 @@ package bench;
  *  3) Copy runs: 5 time: 473
  *     MarkSweepCompact runs: 1 time: 419
  *
- *  4) G1 Young Generation  runs:31   time:1385
- *     G1 Old Generation    runs:7   time:4387
+ *  4) G1 Young Generation  runs:24   time:883
+ *     G1 Old Generation    runs:4   time:1235
  */
 
 import com.sun.management.GarbageCollectionNotificationInfo;
